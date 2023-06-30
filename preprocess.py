58f1475418aa35153705840057567eb42063ed9e
matteusgui <matteusgui@hotmail.com>
2023-06-30 10:21:46 -0300

    Inserção do caracter / no regex e troca para "" O regex para a parte dos partidos agora inclui o caracter / Necessário pois pode acontecer a estrutura NOME (partido/bloco - UF) Viu-se portanto a necessidade de realizar a inserção de tal caracter Retirada de 'senhor' facilita o processamento dos dados.

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import re
 
 def preprocess(lista_discursos: list()) -> list():
     discursos = [discurso.replace("\r\n", "\n") for discurso in lista_discursos]
-    discursos = [re.sub(r"\b[A-Z]+(?:\s+[A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ'.]+)*\s\([\s\w\-\.]+\)\s\-\s|\b[A-Z]+(?:\s+[A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ'.]+)*\s\([\w\s]+\.\s\w+\s\-\s\w+\)\s\-\s", "", discurso) for discurso in discursos]
-    discursos = [re.sub(r"\b[Ss]r[as]*\.", "senhor", discurso) for discurso in discursos]
+    discursos = [re.sub(r"\b[A-Z]+(?:\s+[A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ'.]+)*\s\([\s\w\-\./]+\)\s\-\s|\b[A-Z]+(?:\s+[A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ'.]+)*\s\([\w\s]+\.\s\w+\s\-\s\w+\)\s\-\s", "", discurso) for discurso in discursos]
+    discursos = [re.sub(r"\b[Ss]r[as]*\.", "", discurso) for discurso in discursos]
     discursos = [re.sub(r"\d+(?:[.,]\d+)*", "", discurso) for discurso in discursos]
     return discursos
